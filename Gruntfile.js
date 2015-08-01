cae06418b2d5b5b558516c9309936ef510b8a45e
johnellis1392 <john.ellis1392@gmail.com>
2015-08-01 16:42:36 -0700

    Reorganizing

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+
+/**
+ * Grunt Build File
+ */
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasts('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['uglify']);
+};
+
+
