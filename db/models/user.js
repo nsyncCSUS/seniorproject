3794f6786d7123b280649ef359eef2e41d149a2f
Anthony Nguyen <anthonyn916@yahoo.com>
2015-08-07 12:29:16 -0700

    app.js 	1. Fixed mongoose 	2. Moved models to ./db/models folder user.jade 	3. Removed container 	4. Removed html class, replaced with jade class syntax style.css 	5. Removed global body padding 	6. Added top padding to acommodate top navbar

---
+++ db/models/user.js
@@ -0,0 +1,44 @@
+var mongoose = require('mongoose');
+
+module.exports = mongoose.model('User', {
+	fb : {
+		id : String,
+		access_token : String,
+		firstName : String,
+		lastName : String,
+		email : String
+	},
+	twitter : {
+		id : String,
+		token : String,
+		username : String,
+		displayName : String,
+		lastStatus : String
+	},
+
+	user : {
+		FirstName : String,
+		MiddleName : String,
+		LastName : String,
+		Description : String,
+		Email : String,
+		Birthday : Date,
+		Age : Number,
+		City : String,
+		State : String,
+		ZipCode : Number,
+		PhoneNum : Number,
+		Picture : String,
+		VolunteeredTo : String,
+		CreatorOf : String,
+		OrganizerOf : String,
+		SubscribedTo : String,
+		GooglePlus : String,
+		Facebook : String,
+		LinkenIn : String,
+		Twitter : String,
+		Interests : String,
+		Skills : String
+	}
+
+});
