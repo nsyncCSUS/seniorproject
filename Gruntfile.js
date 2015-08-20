3a6ca3149dd467dda0aae96b45d3b220ce62760a
johnellis1392 <john.ellis1392@gmail.com>
2015-08-20 00:27:07 -0700

    Adding beginning test files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,32 @@
-
 /**
  * Grunt Build File
  */
 
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    }
-  });
+    grunt.loadTasks('test/tasks');
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+                    //'src/<%= pkg.name %>.js': ['build/<%= pkg.name %>.min.js']
+            }
+        },
+        insert: {
+            "data.1": "test/data/data.1.json",
+            "data.2": "test/data/data.2.json"
+        },
+        delete: {
+            "data.1": "test/data/data.1.json",
+            "data.2": "test/data/data.2.json"
+        }
+    });
 
-  grunt.loadNpmTasts('grunt-contrib-uglify');
+    grunt.loadNpmTasts('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['uglify']);
 };
-
-
