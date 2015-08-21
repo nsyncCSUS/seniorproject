9640195e708ee42c2d3f685f52de19182ecb2e00
Anthony Nguyen <anthonyn916@yahoo.com>
2015-08-20 21:40:57 -0700

    Added routing for some restful stuff 	- ie. 		/users/123 		/groups/456 		/events/789 Commented out some problematic code in users.jade

--- public/js/controllers/GroupsController.js
+++ public/js/controllers/GroupsController.js
@@ -1,7 +1,9 @@
 (function() {
 	var app = angular.module('groupsController', [ 'groupService', 'groupFactory' ]);
 
-	app.controller('GroupsController', [ '$scope', 'GroupService', 'GroupFactory', function($scope, GroupService, GroupFactory) {
+	app.controller('GroupsController', [ '$scope', '$routeParams', 'GroupService', 'GroupFactory', function($scope, $routeParams, GroupService, GroupFactory) {
+		
+		$scope.groupId = $routeParams.groupId;
 
 	} ]);
 
