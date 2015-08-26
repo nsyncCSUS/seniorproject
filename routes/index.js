98fb3ba9e5c7730958b09646b289c6d676c7b6e6
johnellis1392 <john.ellis1392@gmail.com>
2015-08-25 20:59:22 -0700

    Adding UsersController

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,96 @@
 var express = require('express');
 var router = express.Router();
 
+// Import controllers
+var UsersController = require('./users.js'); 
+
+var Controllers = {
+  users: new UsersController(), 
+};
+
+/**
+ * John
+ *
+ * Route root requests to controllers. 
+ */
+router.get('/:controller', function(request, response) {
+  var controller = Controllers[request.params.controller]; 
+  controller.get(request, response);
+}); 
+
+/**
+ * John
+ *
+ * Route specific get requests to controller. 
+ */
+router.get('/:controller/:id', function(request, response) {
+  var controller = Controllers[request.params.controller]
+  controller.get(request, response); 
+}); 
+
+
+/**
+ * John
+ *
+ * Route Post requests to controllers 
+ */
+router.post('/:controller', function(request, response) {
+  var controller = Controllers[request.params.controller]; 
+  controller.post(request, response); 
+}); 
+
+
+/**
+ * John
+ *
+ * Route Update requests to controllers 
+ */
+router.put('/:controller/:id', function(request, response) {
+  var controller = Controllers[request.params.controller]; 
+  controller.put(request, response); 
+}); 
+
+
+/**
+ * John
+ *
+ * Route Delete requests to contorllers
+ */
+router.delete('/:controller/:id', function(request, response) {
+  var controller = Controllers[request.params.controller]; 
+  controller.delete(request, response); 
+}); 
+
+
+router.get('/:controller1/:id1/:controller2', function(request, response) {
+  console.log('GET ' + request.params.controller1 + ', ' + request.params.controller2); 
+  var controller = Controllers[request.params.controller1]; 
+  controller[request.params.controller2].get(request, response); 
+}); 
+
+router.get('/:controller1/:id1/:controller2/:id2', function(request, response) {
+  var controller = Controllers[request.params.controller1]; 
+  controller[request.params.controller2].get(request, response); 
+}); 
+
+router.post('/:controller1/:id1/:controller2', function(request, response) {
+  var controller = Controllers[request.params.controller1]; 
+  controller[request.params.controller2].post(request, response); 
+}); 
+
+router.put('/:controller1/:id1/:controller2/:id2', function(request, response) {
+  var controller = Controllers[request.params.controller1]; 
+  controller[request.params.controller2].put(request, response); 
+}); 
+
+router.delete('/:controller1/:id1/:controller2/:id2', function(request, response) {
+  var controller = Controllers[request.params.controller1]; 
+  controller[request.params.controller2].delete(request, response); 
+}); 
+
+
+
+
 router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Senior Project' });
 });
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,143 @@
 var express = require('express');
 var router = express.Router();
 
+function UsersController() {
+  //console.log("Initializing Users Controller..."); 
+};
 
-module.exports = router;
+
+var testUser = {
+  id: 1, 
+  fname: "Test", 
+  lname: "User", 
+  events: [ 
+    {id: 1, name: "Test Event 1"},
+    {id: 2, name: "Test Event 2"}
+  ], 
+
+  groups: [ 
+    {id: 1, name: "Test Group 1"},
+    {id: 2, name: "Test Group 2"}
+  ],
+}; 
+
+
+/**
+ * John
+ *
+ * Collection of functions to do with routing Users data.
+ *
+ * NOTE: Replace later with Mongoose calls. 
+ */
+UsersController.prototype = {
+  get: function(request, response) {
+    var id = request.params.id; 
+    var data = {}; 
+    if(id !== null && id !== undefined) {
+      // Perform actions to do with this specific user. 
+      if(testUser.id === id) {
+        data = testUser; 
+      }
+    } else {
+      data = [testUser]; 
+    }
+
+    response.send(data); 
+  },
+
+  post: function(request, response) {
+    response.send({message: "Hello"}); 
+  },
+
+  put: function(request, response) {
+    response.send({}); 
+  }, 
+
+  delete: function(request, response) {
+    response.send({}); 
+  }, 
+
+  /**
+   * John
+   *
+   * Secondary controller for handling nested actions. 
+   */
+  events: {
+    get: function(request, response) {
+      var id1 = request.params.id1; 
+      var id2 = request.params.id2; 
+      var data = {}; 
+      if(id1 == null || id1 == undefined) {
+        // No user specified; Return illegal access 
+      } else if(id1 == testUser.id) {
+        if(id2 == null || id2 == undefined) {
+          data = testUser.events || {}; 
+        } else {
+          data = testUser.events.filter(function(item) {return item.id == id2;}); 
+        }
+      } else {
+        // Return 404 
+      }
+
+      response.send(data); 
+    },
+
+    post: function(request, response) {
+      response.send({}); 
+    },
+
+    put: function(request, response) {
+      response.send({}); 
+    }, 
+
+    delete: function(request, respons) {
+      response.send({}); 
+    },
+  },
+
+
+  /**
+   * John
+   *
+   * Intermediary controller for handling actions to 
+   * do with a user's subscribed Groups. 
+   */
+  groups: {
+    get: function(request, response) {
+      var id1 = request.params.id1; 
+      var id2 = request.params.id2; 
+      var data = {}; 
+      if(id1 == null || id1 == undefined) {
+        // No user specified: Illegal access 
+        // Return 404
+      } else if(id1 == testUser.id) {
+        if(id2 == null || id2 == undefined) {
+          // Request for all group 
+          data = testUser.groups || {}; 
+        } else {
+          // Request for specific groups 
+          data = testUser.groups.filter(function(item) {return item.id == id2;}); 
+        }
+      } else {
+        // return 404 
+      }
+
+      response.send(data); 
+    }, 
+
+    post: function(request, response) {
+      response.send({}); 
+    }, 
+
+    put: function(request, response) {
+      response.send({}); 
+    }, 
+
+    delete: function(request, response) {
+      response.send({}); 
+    }, 
+  },
+};
+
+module.exports = UsersController;
+//module.exports = router;
