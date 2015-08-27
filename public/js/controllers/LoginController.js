f0cfd309a67fad58a54aebfde0cc5f534ecc2efc
Rick Grimes <4spamhello@gmail.com>
2015-08-26 19:06:24 -0700

    Angular(client side) Authentication

--- public/js/controllers/LoginController.js
+++ public/js/controllers/LoginController.js
@@ -1,5 +1,7 @@
 //HL Posts to /user/login and passes scope.user(this will be passed to 'req.body' inside the POST) when user logins
 // if login is successful extracts the token that the the POST passes back(POST passes back an object)
+// After user is confimed logged in logged service is used to change the isLogged Value
+// This will be used for client sided authentication
 
 //$window is the window object inside the browser
 (function() {
@@ -14,6 +16,7 @@
 				$window.sessionStorage.token = response.data.token;
 				console.log($window.sessionStorage);
 				$location.path('/home');
+				LoginService.isLogged = 'True';
 			});
 		};
 	} ]);
--- public/js/seniorProjectApp.js
+++ public/js/seniorProjectApp.js
@@ -2,7 +2,19 @@
 
 	var app = angular.module('app', [ 'ngRoute', 'ngAnimate', 'homeController', 'dashboardController',
 	                                  'usersController', 'groupsController', 'eventsController', 'signupController',
-	                                  'loginController']);
+																		'loginController']);
+// Uses the method .on to detect if an angular route has beend changed if that is the case
+// run the condition, checks the route access and also checks if the use is logged in
+	app.run(function(LoginService,$rootScope,$location) {
+		$rootScope.$on('$routeChangeStart', function(e, curr, prev){
+			console.log(curr);
+			console.log(prev);
+			console.log(prev.access.isFree );
+			if (!curr.access.isFree && !LoginService.isLogged) {
+					  $location.path('/login');
+				 }
+	});
+});
 
 	app.config([ '$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 
@@ -12,55 +24,94 @@
 		})
 		.when('/home', {
 			templateUrl : '/partials/home.jade',
-			controller : 'HomeController'
+			controller : 'HomeController',
+			access: {
+			isFree: false
+		}
 		})
 		.when('/dashboard', {
 			templateUrl : '/partials/dashboard.jade',
-			controller : 'DashboardController'
+			controller : 'DashboardController',
+			access: {
+			isFree: false
+		}
 		})
 		.when('/users', {
 			templateUrl : '/partials/users.jade',
-			controller : 'UsersController'
+			controller : 'UsersController',
+			access: {
+			isFree: false
+		}
 		})
 		.when('/users/:userId', {
 			templateUrl : '/partials/users.jade',
-			controller : 'UsersController'
+			controller : 'UsersController',
+			access: {
+			isFree: false
+		}
 		})
 		.when('/groups', {
 			templateUrl : '/partials/groups.jade',
-			controller : 'GroupsController'
+			controller : 'GroupsController',
+			access: {
+			isFree: false
+		}
 		})
 		.when('/groups/:groupId', {
 			templateUrl : '/partials/groups.jade',
-			controller : 'GroupsController'
+			controller : 'GroupsController',
+			access: {
+			isFree: false
+		}
 		})
 		.when('/events', {
 			templateUrl : '/partials/events.jade',
-			controller : 'EventsController'
+			controller : 'EventsController',
+			access: {
+			isFree: false
+		}
 		})
 		.when('/events/:eventId', {
 			templateUrl : '/partials/events.jade',
-			controller : 'EventsController'
+			controller : 'EventsController',
+			access: {
+			isFree: false
+		}
 		})
 		.when('/signup', {
 			templateUrl : '/partials/signup.jade',
 			controller : 'SignupController',
+			access: {
+			isFree: true
+		}
 		})
 		.when('/login', {
 			templateUrl : '/partials/login.jade',
-			controller : 'LoginController'
+			controller : 'LoginController',
+			access: {
+			isFree: true
+		}
 		})
 		.when('/logout', {
 			templateUrl : '/partials/logout.jade',
-			controller : 'LogoutController'
+			controller : 'LogoutController',
+			access: {
+			isFree: true
+		}
 		})
 		.when('/create/group', {
 			templateUrl : '/partials/createGroup.jade',
-			controller : 'CreateGroupController'
+			controller : 'CreateGroupController',
+			access: {
+			isFree: false
+		}
 		})
 		.when('/create/event', {
 			templateUrl : '/partials/createEvent.jade',
-			controller : 'CreateEventController'
+			controller : 'CreateEventController',
+			access: {
+			isFree: false
+		}
 		})
 		.otherwise({
 			redirectTo : '/home'
