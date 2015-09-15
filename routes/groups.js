2695f3bceb1b42df243b000604b2243bb0bfdd3f
Anthony Nguyen <anthonyn916@yahoo.com>
2015-09-14 22:48:32 -0700

    Kittens Everywhere
    - Group
    	- Added more dummy data
    		- Changed name to firstName and lastName
    	- Placeholder image will show if user/group/event doesn't have picture
    		- Subscribers without images will be listed at end of list
    	- Added some .row to align some parts of groups page
    	- Capitalized Interest Names
    	- Any type of user will have a tooltip above their picture on hover
    	- Events and users are now clickable (name + picture)
    
    - Create Group
    	- Placeholder + dummy data for creator to be an organizer
    	- Added placeholder for inviting organizers

--- routes/groups.js
+++ routes/groups.js
@@ -7,7 +7,7 @@ router.get('/data/:id', function(req, res) {
 	{
 			id : "nsync",
 			name : "N.Sync()",
-			picture : "//placehold.it/500x500",
+			picture : "//placekitten.com/g/500/500/",
 			creationDate : "2015-08-26T18:50:10.111Z",
 			city : "Sacramento",
 			state : "CA",
@@ -20,50 +20,66 @@ router.get('/data/:id', function(req, res) {
 			events : [ {
 				id : "event1",
 				name : "event1",
-				interests : [{type: "animals"}, {type: "education"}, {type: "environment"}, {type: "people"}, {type: "recreation"}, {type: "technology"}, {type: "youth"}],
-				volunteers: [{id: "v1", name: "v1", picture: "//placehold.it/250x250"}]
+				picture : "//placekitten.com/g/501/500/",
+				startTimeDate : "2015-08-26T18:50:10.111Z",
+				endTimeDate : "2015-08-27T18:50:10.111Z",
+				maxVolunteers : 50,
+				interests : [{type: "Animals"}, {type: "Education"}, {type: "Environment"}, {type: "People"}, {type: "Recreation"}, {type: "Technology"}, {type: "Youth"}],
+				volunteers: [{id: "v1", firstName: "Kitten 1", lastName: "1"}, 
+				             {id: "v2", firstName: "Kitten 2", lastName: "1", picture: "//placekitten.com/g/250/251"}, 
+				             {id: "v3", firstName: "Kitten 3", lastName: "1"}, 
+				             {id: "v4", firstName: "Kitten 4", lastName: "1", picture: "//placekitten.com/g/250/253"}, 
+				             {id: "v5", firstName: "Kitten 5", lastName: "1", picture: "//placekitten.com/g/250/254"}, 
+				             {id: "v6", firstName: "Kitten 6", lastName: "1", picture: "//placekitten.com/g/250/255"}, 
+				             {id: "v7", firstName: "Kitten 7", lastName: "1", picture: "//placekitten.com/g/250/256"}, 
+				             {id: "v8", firstName: "Kitten 8", lastName: "1", picture: "//placekitten.com/g/250/257"}, 
+				             {id: "v9", firstName: "Kitten 9", lastName: "1", picture: "//placekitten.com/g/250/258"}, 
+				             {id: "v10", firstName: "Kitten 10", lastName: "1", picture: "//placekitten.com/g/250/259"}, 
+				             {id: "v11", firstName: "Kitten 11", lastName: "1", picture: "//placekitten.com/g/250/260"}]
 			},
 			{
 				id : "event2",
 				name : "event2",
-				interests : [{type: "animals"}, {type: "education"}, {type: "environment"}, {type: "people"}, {type: "recreation"}, {type: "technology"}, {type: "youth"}]
+				picture : "//placekitten.com/g/503/500/",
+				startTimeDate : "2015-08-28T18:50:10.111Z",
+				endTimeDate : "2015-08-29T18:50:10.111Z",
+				maxVolunteers : 50,
+				interests : [{type: "Animals"}, {type: "Education"}, {type: "Environment"}, {type: "People"}, {type: "Recreation"}],
+				volunteers: [{id: "v1", firstName: "Kitten 1", lastName: "1", picture: "//placekitten.com/g/251/250"}, 
+				             {id: "v2", firstName: "Kitten 2", lastName: "1", picture: "//placekitten.com/g/251/251"}, 
+				             {id: "v3", firstName: "Kitten 3", lastName: "1", picture: "//placekitten.com/g/251/252"}, 
+				             {id: "v4", firstName: "Kitten 4", lastName: "1", picture: "//placekitten.com/g/251/253"}, 
+				             {id: "v5", firstName: "Kitten 5", lastName: "1"}, 
+				             {id: "v6", firstName: "Kitten 6", lastName: "1", picture: "//placekitten.com/g/251/255"}, 
+				             {id: "v7", firstName: "Kitten 7", lastName: "1", picture: "//placekitten.com/g/251/256"}, 
+				             {id: "v8", firstName: "Kitten 8", lastName: "1", picture: "//placekitten.com/g/251/257"}, 
+				             {id: "v9", firstName: "Kitten 9", lastName: "1", picture: "//placekitten.com/g/251/258"}, 
+				             {id: "v10", firstName: "Kitten 10", lastName: "1"}, 
+				             {id: "v11", firstName: "Kitten 11", lastName: "1", picture: "//placekitten.com/g/251/260"}, 
+				             {id: "v12", firstName: "Kitten 12", lastName: "1", picture: "//placekitten.com/g/251/261"}, 
+				             {id: "v13", firstName: "Kitten 13", lastName: "1", picture: "//placekitten.com/g/251/262"}, 
+				             {id: "v14", firstName: "Kitten 14", lastName: "1", picture: "//placekitten.com/g/251/263"}, 
+				             {id: "v15", firstName: "Kitten 15", lastName: "1", picture: "//placekitten.com/g/251/264"}, 
+				             {id: "v16", firstName: "Kitten 16", lastName: "1", picture: "//placekitten.com/g/251/265"}]
 			}
 			],
-			organizers : [ {
-				id : "org1",
-				name : "org1"
-			},{
-				id : "org2",
-				name : "org2"
-			},{
-				id : "org3",
-				name : "org3"
-			},{
-				id : "org4",
-				name : "org4"
-			},{
-				id : "org5",
-				name : "org5"
-			} ],
-			subscribers : [ {
-				id : "sub1",
-				name : "sub1"
-			},{
-				id : "sub2",
-				name : "sub2"
-			},{
-				id : "sub3",
-				name : "sub3"
-			},{
-				id : "sub4",
-				name : "sub4"
-			},{
-				id : "sub5",
-				name : "sub5"
-			} ],
-			interests : [ {
-				type : "technology"
-			}, ]
+			organizers : [{id : "org1", firstName : "org1", lastName: "1"},
+			              {id : "org2", firstName : "org2", lastName: "1", picture : "//placekitten.com/g/351/350/"},
+			              {id : "org3", firstName : "org3", lastName: "1", picture : "//placekitten.com/g/352/350/"},
+			              {id : "org4", firstName : "org4", lastName: "1", picture : "//placekitten.com/g/353/350/"},
+			              {id : "org5", firstName : "org5", lastName: "1"},
+			              {id : "org6", firstName : "org6", lastName: "1", picture : "//placekitten.com/g/355/350/"}],
+			subscribers : [{id : "sub1", firstName : "sub1", lastName: "1", picture : "//placekitten.com/g/350/355/"},
+			               {id : "sub2", firstName : "sub2", lastName: "1", picture : "//placekitten.com/g/351/355/"},
+			               {id : "sub3", firstName : "sub3", lastName: "1", picture : "//placekitten.com/g/352/355/"},
+			               {id : "sub4", firstName : "sub4", lastName: "1"},
+			               {id : "sub5", firstName : "sub5", lastName: "1", picture : "//placekitten.com/g/354/355/"},
+			               {id : "sub6", firstName : "sub6", lastName: "1", picture : "//placekitten.com/g/355/355/"},
+			               {id : "sub7", firstName : "sub7", lastName: "1", picture : "//placekitten.com/g/350/355/"},
+			               {id : "sub8", firstName : "sub8", lastName: "1", picture : "//placekitten.com/g/356/355/"},
+			               {id : "sub9", firstName : "sub9", lastName: "1"},
+			               {id : "sub10", firstName : "sub10", lastName: "1", picture : "//placekitten.com/g/358/355/"}],
+			interests : [{type: "Animals"}, {type: "Education"}, {type: "Environment"}, {type: "People"}, {type: "Recreation"}, {type: "Technology"}, {type: "Youth"}]
 	};
 	
 	console.log("Got a GET request for /data/groups/" + req.params.id);
