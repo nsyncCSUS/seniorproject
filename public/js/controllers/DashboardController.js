29248782d8343aa841398ff11a85986acd1bf924
Vadzim Savenok <vsavenok@yahoo.com>
2015-09-14 18:43:29 -0700

    Poppulated controller with data

--- public/js/controllers/DashboardController.js
+++ public/js/controllers/DashboardController.js
@@ -1,8 +1,41 @@
 (function() {
-	var app = angular.module('dashboardController', ['dashboardService', 'dashboardFactory']);
-
-	app.controller('DashboardController', [ '$scope', 'DashboardService', 'DashboardFactory', function($scope, DashboardService, DashboardFactory) {
-
+	var app = angular.module('dashboardController', ['dashboardService', 'dashboardFactory']);	//creating new module
+	
+	/*create new controller; [] is a list of dependencies to fetch, and pass them all into a constructor function*/
+	app.controller('DashboardController', [ '$scope', 'DashboardService', 'DashboardFactory', 'EventService', function($scope, DashboardService, DashboardFactory, EventService) {
+		//put all logic in controller
+		//$scope will stores data
+		//$scope.message = "A new message";
+		//$scope.event = {startTimeDate:"string"}	//will store startTimeDate inside of event, and it will store event inside of scope
+		$scope.event = {startTimeDate: event.startTimeDate}	//will store startTimeDate inside of event, and it will store event inside of scope
+		
+		
+		// Call service function with a callback. The first argument 
+		// is the data you want to pass to the Http request, and the 
+		// second argument is a function to be called when the Http
+		// request returns with a successful status code. Then reassign
+		// $scope.event to the new event object we receive from the Http
+		// response. 
+		
+		/* EventService.getEvent({id: 1}, function(response) {	
+			$scope.event = response.event; 
+		
+		});
+		EventService.getEvent({startTimeDate: event.startTimeDate}, function(response){
+			$scope.event = response.event;
+		}); */
+		
+		$scope.event = {
+			startTimeDate: '09/12/2015', 
+			endTimeDate: '09/24/2015',
+			yourEventsDate: '09/14/2015',
+			yourEventsTime: '12:00PM',
+			yourGroupEventsDate: '09/15/2015',
+			yourGroupEventsTime: '3:00PM',
+			volunteerList: [{name: 'John'}],
+			yourGroupEvents: [{groupEventTitle: 'Event 1', time: 'Time 1', date: 'Date 1'},
+				{groupEventTitle: 'Event 2', time: 'Time 2', date: 'Date 2'}]
+		}
 	} ]);
 
 })();
