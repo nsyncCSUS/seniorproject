13c7773595ea17423e782f11327dd6cd99ee98a0
shanessingh <college711@hotmail.com>
2015-10-03 11:15:30 -0700

    All changes done to the users and createEvent.jade

--- public/js/controllers/UsersController.js
+++ public/js/controllers/UsersController.js
@@ -2,28 +2,12 @@
 	var app = angular.module('usersController', [ 'userService', 'userFactory' ]);
 
 	app.controller('UsersController', [ '$scope', '$routeParams', 'UserService', 'UserFactory', function($scope, $routeParams, UserService, UserFactory) {
-
+        /******************************************************************
+		/ Variables
+		******************************************************************/
 		$scope.userId = $routeParams.userId;
 		
-		// Problematic code
-		/*		
-		var refresh = function(){
-			UserService.get('/users').success(function(response)
-			{
-				console.log("I got the data");
-			});
-		};
-		refresh();
-		 */
 		
-		function refresh(){
-			// Problematic code
-			//UserService.get('/users').success(function test(response)
-			//{
-				console.log("I got the data");
-			//});
-		};
-		refresh();
 	
 	
 	}]);
