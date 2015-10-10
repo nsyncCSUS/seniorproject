afc39431d21871c83d21b61b74452de5e965cafc
Kristopher Tadlock <kristophertadlock@gmail.com>
2015-10-10 12:56:51 -0700

    Committed

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -1,18 +1,122 @@
 (function() {
     var app = angular.module('homeController', ['homeService', 'homeFactory']);
 
+
     app.controller('HomeController', ['$scope', 'HomeService', 'HomeFactory', function($scope, HomeService, HomeFactory) {
 
         /***************************************************************************
          * Variables (includes ones from scope too)
          **************************************************************************/
-        this.isSearching = false; // Default
-        this.currentTab;
+        $scope.isSearching = false; // Default
+        $scope.currentTab = 'events';
 
         $scope.searchtext;
         $scope.usersSearchResults;
         $scope.groupsSearchResults;
         $scope.eventsSearchResults;
+        $scope.searchbox = '';
+        $scope.descrip = "center sodales malesuada accumsan vel, condimentum eget eros. Mauris consectetur nisi in ex pharetra commodo. Nullam aliquam velit sem, nec molestie risus eleifend ac. In fringilla, nisl ac gravida convallis, turpis eros accumsan urna, sed molestie tortor libero sit amet lacus. Nulla porttitor euismod purus, ut hendrerit leo vehicula sed. Aenean ad";
+
+        $scope.events = [{
+        EventName: "American River Cleanup!",
+        
+        Group: {
+            GroupName:"Concerned Citizens for Rivers",
+        },
+        
+        Description: $scope.descrip,
+        CreationDate: "2015-08-26T18:50:10.111Z",
+        StartTimeDate: "2015-08-26T18:50:10.111Z",
+        EndTimeDate: "2015-08-26T18:50:10.111Z",
+        Address: "1234 Fake Street",
+        City: "Sacramento",
+        State: "CA",
+        Zipcode: "95621",
+        
+        VolunteerList: [{
+        }],
+        
+        CreationUser: {
+        },
+        
+        MaxVolunteers: 200,
+        Interests: ['environment', 'people', 'youth'] 
+        },
+        {
+       	EventName: "American River Cleanup!",
+        
+        Group: {
+            GroupName:"Concerned Citizens for Rivers",
+        },
+        
+        Description: $scope.descrip,
+        CreationDate: "2015-08-26T18:50:10.111Z",
+        StartTimeDate: "2015-08-26T18:50:10.111Z",
+        EndTimeDate: "2015-08-26T18:50:10.111Z",
+        Address: "1234 Fake Street",
+        City: "Sacramento",
+        State: "CA",
+        Zipcode: "95621",
+        
+        VolunteerList: [{
+        }],
+        
+        CreationUser: {
+        },
+        
+        MaxVolunteers: 200,
+        Interests: ['environment', 'people', 'youth'] 
+        }, 
+        {
+       	EventName: "American River Cleanup!",
+        
+        Group: {
+            GroupName:"Concerned Citizens for Rivers",
+        },
+        
+        Description: $scope.descrip,
+        CreationDate: "2015-08-26T18:50:10.111Z",
+        StartTimeDate: "2015-08-26T18:50:10.111Z",
+        EndTimeDate: "2015-08-26T18:50:10.111Z",
+        Address: "1234 Fake Street",
+        City: "Sacramento",
+        State: "CA",
+        Zipcode: "95621",
+        
+        VolunteerList: [{
+        }],
+        
+        CreationUser: {
+        },
+        
+        MaxVolunteers: 200,
+        Interests: ['environment', 'people', 'youth'] 
+    	},
+    	{
+    	 EventName: "American River Cleanup!",
+        
+        Group: {
+            GroupName:"Concerned Citizens for Rivers",
+        },
+        
+        Description: $scope.descrip,
+        CreationDate: "2015-08-26T18:50:10.111Z",
+        StartTimeDate: "2015-08-26T18:50:10.111Z",
+        EndTimeDate: "2015-08-26T18:50:10.111Z",
+        Address: "1234 Fake Street",
+        City: "Sacramento",
+        State: "CA",
+        Zipcode: "95621",
+        
+        VolunteerList: [{
+        }],
+        
+        CreationUser: {
+        },
+        
+        MaxVolunteers: 200,
+        Interests: ['environment', 'people', 'youth']
+    }];
 
         /***************************************************************************
          * Initialize the search for users, groups, events based on keywords
@@ -22,113 +126,129 @@
          **************************************************************************/
         $scope.search = function(searchbox) {
             if (searchbox.length > 0) {
-                this.isSearching = true;
+                $scope.isSearching = true;
 
                 $scope.searchtext = searchbox;
-                $scope.searchbox = '';
-                this.currentTab = 'events';
+                //$scope.searchbox = '';
+                $scope.currentTab = 'events';
 
                 // Get Events
                 $scope.eventsSearchResults = HomeService.getEventSearchResults();
                 // Get users
-                $scope.usersSearchResults = HomeService.getUserSearchResults();
+ //               $scope.usersSearchResults = HomeService.getUserSearchResults();
                 // Get Groups
-                $scope.groupsSearchResults = HomeService.getGroupSearchResults();
+//                $scope.groupsSearchResults = HomeService.getGroupSearchResults();
             }
-        }
+        };
+
+        $scope.getSearchBox = function(){
+        	if (typeof searchbox !== 'undefined'){
+        		return searchbox;
+        	}
+        	else{
+        		return '';
+        	}
+        };
 
         /***************************************************************************
          * Functions that controls the view for searching or not search
          **************************************************************************/
         $scope.searching = function() {
-            if (this.isSearching == true)
+            if ($scope.isSearching == true)
                 return true;
             else
                 return false;
-        }
+        };
         $scope.stopSearching = function() {
-            this.isSearching = false;
-        }
+            $scope.isSearching = false;
+        };
 
 
-
+        $scope.stub = function(){return true;};
         /***************************************************************************
          * Functions that controls tabs for searching
          **************************************************************************/
         $scope.setCurrentTab = function(category) {
-            this.currentTab = category;
-        }
+            $scope.currentTab = category;
+        };
 
         $scope.getCurrentTab = function(category) {
-            if (this.currentTab === category)
+            if ($scope.currentTab === category)
                 return true;
             else
                 return false;
-        }
+        };
 
     }]);
 
 })();
-
 /*
-	user: {
-		firstName : 	String,
-		middleName : 	String,
-		lastName : 		String,
-		description : 	String,
-		picture:		String,
-		email : 		String,
-		birthday : 		Date,
-		age : 			Number,
-		location :		{city: String, state: String, zipcode: String},	
-		phoneNum : 		Number,
-		googlePlus : 	String,
-		facebook : 		String,
-		linkedIn : 		String,
-		twitter : 		String,
-		volunteeredTo : [{id: String}, {id: String}, ...],
-		creatorOf : 	[{id: String}, {id: String}, ...],
-		organizerOf : 	[{id: String}, {id: String}, ...],
-		subscribedTo : 	[{id: String}, {id: String}, ...],
-		interests : 	[{type: String}, {type: String}, ...]
-	}
+info: {
+	firstName : 	String,
+	middleName : 	String,
+	lastName : 		String,
+	description : 	String,
+	email : 		String,
+	birthday : 		Date,
+	age : 			Number,
+	city : 			String,
+	state : 		String,
+	zipCode : 		Number,
+	phoneNum : 		Number,
+	picture : 		String,
+	googlePlus : 	String,
+	facebook : 		String,
+	linkedIn : 		String,
+	twitter : 		String,
+	volunteeredTo : [{id: String}, {id: String}, ...],
+	creatorOf : 	[{id: String}, {id: String}, ...],
+	organizerOf : 	[{id: String}, {id: String}, ...],
+	subscribedTo : 	[{id: String}, {id: String}, ...],
+	interests : 	[{type: String}, {type: String}, ...]
+}
 */
 
 /*
-	group: {
+	info: {
 		id : 				String,
-		name : 				String,
+		groupName : 		String,
 		picture : 			String,
 		creationDate : 		String,
-		location :			[{city: String, state: String, zipcode: String}, ...],
+			//city : 			String,
+			//state : 			String,
+			//zipCode : 		Number,
 		description : 		String,
 		googlePlusURL : 	String,
 		facebookURL : 		String,
 		linkInURL : 		String,
 		twitterURL: 		String,
 		personalWebsiteURL: String,
-		events:				[{id: String}, {id: String}, ...],
-		organizers:			[{id: String}, {id: String}, ...],
+		events:			[{id: String}, {id: String}, ...],
+		organizers:		[{id: String}, {id: String}, ...],
 		subscribers:		[{id: String}, {id: String}, ...],
-		interests: 			[{type: String}, {type: String}, ...]
+		interests: 	[{type: String}, {type: String}, ...]
 
 	}
 */
 
 /*
-	event: {
-		id: 			String,		
-		creatorId: 		String,
-		groupId: 		String,
-		name: 			String,
-		description: 	String,
-		picture: 		String,
-		creationDate: 	DateTime,
-		startTimeDate: 	DateTime,
-		endTimeDate: 	DateTime,
-		location :		{street: String, city: String, state: String, zipcode: String},	
-		maxVolunteers: 	Number,
-		volunteers:		[{id: String}, {id: String}, ...],
-		interests: 		[{type: String}, {type: String}, ...]
-	}
+info: {
+	id: 			String,		
+	creatorId: 		String,
+	groupId: 		String,
+	eventName: 		String,
+	description: 	String,
+	picture: 		String,
+	creationDate: 	DateTime,
+	startTimeDate: 	DateTime,
+	endTimeDate: 	DateTime,
+	address: 		String,
+	city: 			String,
+	state: 			String,
+	zipcode: 		Number,		
+	maxVolunteers: String,
+	volunteers:	[{id: String}, {id: String}, ...],
+	interests: [{type: String}, {type: String}, ...]
+	
+}
 */
