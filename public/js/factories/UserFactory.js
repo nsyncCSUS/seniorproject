c135d80eb50a4892f55f78426c7128fdc59a2b3c
HuyLe2 <Lhuy2009@gmail.com>
2015-10-21 18:01:37 -0700

    Improved what is returned from the userFactory now returns post object only. Changed res.end from the search.js routes into res.send() now the responses return the object instead of nothing

--- public/js/factories/UserFactory.js
+++ public/js/factories/UserFactory.js
@@ -5,11 +5,23 @@
   app.factory('UserFactory', ['$http', function($http) {
 
     var factory = {
-      userObject: function(searchWord) {
+      // Note: parameters in post have to be an object
+      // Also name in search.js route uses searchString as name
+      getAllUsers: function() {
         return $http.post('/api/search/getallusers');
+      },
 
+      getAUserByID: function(searchValue){
+        return $http.post('/api/search/getauser',{searchString:searchValue});
+      },
+
+      getAUserByName: function(searchValue){
+        return $http.post('/api/search/getauserbyname',{searchString:searchValue});
       }
+
     };
+
+
 
     return factory;
   }]);
