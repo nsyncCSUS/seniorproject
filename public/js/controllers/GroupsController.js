54d0be465ea698a3f6df8da223481a1ad9ab8fc4
johnellis1392 <john.ellis1392@gmail.com>
2015-11-10 15:39:47 -0800

    Merge anthonysBranch_v2

--- public/js/controllers/GroupsController.js
+++ public/js/controllers/GroupsController.js
@@ -47,6 +47,10 @@
 				$scope.otherTabs[0] = "Upcoming Events";
 				break;
 			}
+			
+			$timeout(function() {
+				$anchorScroll('tabs');
+			}, 1);
 		}
 		
 		$scope.getCurrentTab = function(tabName) {
@@ -582,74 +586,3 @@
 	} ]);
 
 })();
-
-
-/*
-	user: {
-		firstName : 	String,
-		middleName : 	String,
-		lastName : 		String,
-		description : 	String,
-		picture:		String,
-		email : 		String,
-		birthday : 		Date,
-		age : 			Number,
-		city: 			String, 
-		state: 			String, 
-		zipcode: 		String,	
-		phoneNum : 		Number,
-		googlePlusURL : 	String,
-		facebookURL : 		String,
-		linkInURL : 		String,
-		twitterURL: 		String,
-		volunteeredTo : [{id: String}, {id: String}, ...],
-		creatorOf : 	[{id: String}, {id: String}, ...],
-		organizerOf : 	[{id: String}, {id: String}, ...],
-		subscribedTo : 	[{id: String}, {id: String}, ...],
-		interests : 	[String]
-	}
-*/
-
-/*
-group: {
-	id : 				String,
-	name : 				String,
-	picture : 			String,
-	creationDate : 		String,
-	city: 				String, 
-	state: 				String, 
-	zipcode: 			String,
-	description : 		String,
-	googlePlusURL : 	String,
-	facebookURL : 		String,
-	linkInURL : 		String,
-	twitterURL: 		String,
-	personalWebsiteURL: String,
-	events:				[{id: String}, {id: String}, ...],
-	organizers:			[{id: String}, {id: String}, ...],
-	subscribers:		[{id: String}, {id: String}, ...],
-	interests: 			[String]
-
-}
-*/
-
-/*
-event: {
-	id: 			String,		
-	creatorId: 		String,
-	groupId: 		String,
-	name: 			String,
-	description: 	String,
-	picture: 		String,
-	creationDate: 	DateTime,
-	startTimeDate: 	DateTime,
-	endTimeDate: 	DateTime,
-	street: 		String, 
-	city: 			String, 
-	state: 			String, 
-	zipcode: 		String,	
-	maxVolunteers: 	Number,
-	volunteers:		[{id: String}, {id: String}, ...],
-	interests: 		[String]
-}
-*/
